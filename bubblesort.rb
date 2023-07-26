1fbdaf335722d3c3e9dc1b44f71b2ec3566b9ca6
xJGBx <jgboyd2004@gmail.com>
2023-07-25 22:58:16 -0400

    Add all

---
+++ bubblesort.rb
@@ -0,0 +1,18 @@
+array = [4,3,78,2,0,2]
+
+def bubblesort(array)
+    sorted = false
+    until sorted
+      sorted = true
+      (array.length - 1).times do |i|
+        if array[i] > array[i + 1]
+          array[i], array[i + 1] = array[i + 1], array[i]
+          sorted = false
+        end
+      end
+    end
+    array
+  end
+end
+
+puts bubblesort(array)
